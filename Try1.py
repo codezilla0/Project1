d85bc9cf5cd8355ff9773967d35e848a19109291
codezilla0 <35879411+codezilla0@users.noreply.github.com>
2018-04-09 15:39:09 -0700

    ch1

--- Try1.py
+++ Try1.py
@@ -3,6 +3,8 @@
 
 import pandas as pd
 import numpy as np
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
 
 df = pd.read_csv("balance-scale.data", names = ['balance', 'var1', 'var2', 'var3', 'var4'])
 df.head()
@@ -10,3 +12,17 @@ df['balance'].value_counts()
 
 
 df['balance'] = [1 if b == 'B' else 0 for b in df.balance]
+
+type(df.balance)
+df.balance.value_counts()
+type(df['balance'])
+
+y = df.balance
+
+X = df.drop('balance', axis=1)
+
+clf_0 = LogisticRegression().fit(X, y)
+
+pred_clf_0 = clf_0.predict(X)
+
+sum(y - pred_clf_0)
