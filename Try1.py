f73607fcb553714cbd4bc1ef10895bda193b0f6d
codezilla0 <35879411+codezilla0@users.noreply.github.com>
2018-04-09 15:29:09 -0700

    Initial commit

---
+++ Try1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# copied from https://elitedatascience.com/imbalanced-classes
+
+import pandas as pd
+import numpy as np
+
+df = pd.read_csv("balance-scale.data", names = ['balance', 'var1', 'var2', 'var3', 'var4'])
+df.head()
+df['balance'].value_counts()
+
+
